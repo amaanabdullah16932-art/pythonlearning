bff28d71f0d1cd68d2010b626db4e63625b0ec10
amaanabdullah20018843-ctrl <amaanabdullah.20018843@gmail.com>
2025-11-11 15:21:17 +0530

    ADDED NESTED PROGRAM FOR ODD-EVEN

---
+++ even-odd.py
@@ -0,0 +1,8 @@
+num = int(input("ENTER THE NUMBERS"))
+if num >= 0:
+    if num % 2 == 0:
+        print ("this is even")
+    else:
+        print("this is odd")
+else:
+    print("please enter the + no.")
